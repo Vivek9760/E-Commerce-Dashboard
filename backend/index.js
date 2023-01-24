ac481e941cb201ec6f2397dcd689f429a7031f97
VivekSaini <vvivekssaini@gmail.com>
2023-01-25 01:42:23 +0530

    commit

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app =express();
+const mongoose = require('mongoose');
+
+app.get(('/'),(req,res)=>{
+    res.send("It's Working")
+})
+
+app.listen(5000)
---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Nav from './Nav.js';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Nav />
+      <Routes>
+      <Route path='/' element={<h1>Product Listing Component</h1>} />
+      <Route path='/add' element={<h1>Add Component</h1>} />
+      <Route path='/update' element={<h1>Update Component</h1>} />
+      <Route path='/logout' element={<h1>Logout Component</h1>} />
+      <Route path='/profile' element={<h1>Profile Component</h1>} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
