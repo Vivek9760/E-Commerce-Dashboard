37303c3bc08f130827b54000bd55212120d190f5
VivekSaini <vvivekssaini@gmail.com>
2023-01-26 02:32:05 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const app =express();
-// const mongoose = require('mongoose');
+const app = express();
+require('./db/config.js');
+const users = require("./db/user.js");
 
-app.get(('/'),(req,res)=>{
-    res.send("It's Working")
+app.use(express.json());
+
+
+app.get(('/'), async(req,res)=>{
+    let result = await users.find();
+    console.log(result);
+    res.send(result);
 })
 
-app.listen(5000)
+app.post('/register',async(req,res)=>{
+    const data = new users(req.body);
+    const result = data.save();
+    res.send(result);
+    console.log(data);
+})
+app.listen(5000);
