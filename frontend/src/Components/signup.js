d42cb8003739f3ec27aa7b2026c7e10635224b56
VivekSaini <vvivekssaini@gmail.com>
2023-01-26 01:28:35 +0530

    commit

--- frontend/src/Components/signup.js
+++ frontend/src/Components/signup.js
@@ -1,13 +1,35 @@
-const signup = ()=>{
+import { useState } from "react";
+
+
+const Signup = ()=>{
+
+    const[name,setName] = useState("");
+    const[email,setEmail]=useState("");
+    const[password,setPassword] = useState("");
+
+    const handleChange = (e)=>{setName(e.target.value)};
+
     return(
         <div className="register">
             <h2>Register</h2>
-                <input className="input-box" type="text" placeholder="Enter Your Name" />
-                <input className="input-box" type="email" placeholder="Enter Your E-mail" />
-                <input className="input-box" type="text" placeholder="Enter Password" />
-                <input className="app-btn" type="button" value="Sign Up" />
+                <input className="input-box"
+                 value={name} onChange={(e)=>{setName(e.target.value)}}
+                  type="text" placeholder="Enter Your Name" />
+                
+                <input className="input-box"
+                 value={email} onChange={(e)=>{setEmail(e.target.value)}}
+                 type="email" placeholder="Enter Your E-mail" />
+                
+                <input className="input-box"
+                value={password} onChange={(e)=>{setPassword(e.target.value)}}
+                type="password" placeholder="Enter Password" />
+                
+                <input className="app-btn"
+                onClick={()=>{
+                    console.log(name,email,password)
+                }} type="button" value="Sign Up" />
         </div>
     )
 }
 
-export default signup;
+export default Signup;
