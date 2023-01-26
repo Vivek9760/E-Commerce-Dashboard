7578303d1e894082fc6a3bbeae463d2a7a8ae267
VivekSaini <vvivekssaini@gmail.com>
2023-01-27 01:59:02 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,31 @@ require('./db/config.js');
 const user = require("./db/user.js");
 
 app.use(express.json());
-app.use(cors());
+app.use(cors()); 
 
-app.get(('/'), async(req,res)=>{
-    let result = await user.find();
-    console.log(result);
-    res.send(result);
-})
 
 app.post('/register',async(req,res)=>{
     const data = new user(req.body);
-    const result =await data.save();
+    let result =await data.save();
+    result = result.toObject();
+    delete result.password
+
     res.send(result);
     console.log(data);
 })
+
+app.post(('/login'), async(req,res)=>{
+    if(req.body.password && req.body.email){
+    let result = await user.findOne(req.body).select("-password");
+    // console.log(result);
+    if(result){
+        res.send(result);
+    }else{
+        res.send("no user found")
+    }
+}else{
+    res.send("no user found")
+}
+})
+
 app.listen(5000);
