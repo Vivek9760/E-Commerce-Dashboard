a891d73821f61de225ce726a62443d7ab3ae0b7f
VivekSaini <vvivekssaini@gmail.com>
2023-01-28 16:25:31 +0530

    commit

--- frontend/src/Components/AddProduct.js
+++ frontend/src/Components/AddProduct.js
@@ -7,8 +7,17 @@ const AddProduct = () =>{
     const [price,setPrice]=useState('');
     const [category,setCategory]=useState('');
     const [company,setCompany]=useState('');
+    const [error,setError]=useState(false);
 
    const addProduct = async() =>{
+    console.warn(!name)
+
+    if(!name || !price || !category || !company)
+    {
+        setError(true);
+    return false;
+    }
+
     let userId = localStorage.getItem('user');
     userId = JSON.parse(userId)._id;
     console.log(userId);
@@ -26,15 +35,24 @@ const AddProduct = () =>{
    setCategory('');
    setCompany('');
    setPrice('');
+   setError(false);
    }
     
     return(
         <div className="add-product">
             <h1>Add Product</h1>
             <input type="text" className="input-box" value={name} onChange={(e)=>{setName(e.target.value)}} placeholder="Enter product name" />
+            {error && !name && <span className="invalid-input">Enter valid name</span>}
+            
             <input type="text" className="input-box" value={price} onChange={(e)=>{setPrice(e.target.value)}} placeholder="Enter product price" />
+            {error && !price && <span className="invalid-input">Enter valid price</span>}
+            
             <input type="text" className="input-box" value={category} onChange={(e)=>{setCategory(e.target.value)}} placeholder="Enter product category" />
+            {error && !category && <span className="invalid-input">Enter valid category</span>}
+            
             <input type="text" className="input-box" value={company} onChange={(e)=>{setCompany(e.target.value)}} placeholder="Enter product company" />
+            {error && !company && <span className="invalid-input">Enter valid company</span>}
+            
             <input type="button" className="app-btn" onClick={addProduct} value="Add Product" />
         </div>
     )
