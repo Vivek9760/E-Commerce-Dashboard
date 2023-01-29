50ba21c60dfa57c5897964c10e7c417ab04f062c
VivekSaini <vvivekssaini@gmail.com>
2023-01-30 01:28:42 +0530

    commit

--- frontend/src/Components/ProductList.js
+++ frontend/src/Components/ProductList.js
@@ -15,31 +15,48 @@ const ProductList = () => {
         result =await result.json();
         setProducts(result)
     }
- console.log(products)
+
+    const deleteProduct = async(id) => {
+
+        let result = await fetch(`http://localhost:5000/delete/${id}`,{
+            method:"delete"
+        });
+        // console.log(result)
+        if(result){
+        getProducts();
+        }
+      
+    }
 
     return(
         <div className="list-container">
     <table  className="product-list">
         <caption><h1>Product List</h1></caption>
+        <thead>
         <tr className="list-heading">
                 <td className="list-data">S.No.</td>
                 <td className="list-data">Name</td>
                 <td className="list-data">Price</td>
                 <td className="list-data">Category</td>
+                <td className="list-data">Operation</td>
         </tr>
- 
+        </thead>
+
+        <tbody>
         { 
             products.map((item,index)=>{
                 return (
-                <tr>
+                <tr key={item._id}>
                 <td className="list-data">{index+1}</td>
                 <td className="list-data">{item.name}</td>
                 <td className="list-data">{item.price}</td>
                 <td className="list-data">{item.category}</td>
+                <td><button onClick={()=>{deleteProduct(item._id)}}>💥</button></td>
                 </tr>
                 )
             })
         }
+        </tbody>
     </table>
     </div>
     )
