05da0a70d92d7a2bcfa19eb705c8b284440f9e5c
VivekSaini <vvivekssaini@gmail.com>
2023-01-31 03:21:55 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import UpdateProduct from './Components/UpdateProduct';
 function App() {
   return (
     <div>
+      <div className='upper-body'>
       <BrowserRouter>
       <Nav />
       <Routes>
@@ -19,13 +20,14 @@ function App() {
       <Route path='/' element={<ProductList/>} />
       <Route path='/add' element={<AddProduct />} />
       <Route path='/update/:id' element={<UpdateProduct />} />
-      <Route path='/logout' element={<h1>Logout Component</h1>} />
+      {/* <Route path='/logout' element={<h1>Logout Component</h1>} /> */}
       <Route path='/profile' element={<h1>Profile Component</h1>} />
       </Route>
       <Route path='/signup' element={<Signup />} />
       <Route path='/login' element={<Login />} />
       </Routes>
       </BrowserRouter>
+      </div>
       <Footer />
     </div>
   );
